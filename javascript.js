b6f09f72e91865b7c3164904d257cc7ad7bc5461
Juan Zimmerman <158226320+juan-zimmerman@users.noreply.github.com>
2024-05-16 20:22:08 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,50 @@
+// Comentario em uma linha
+
+/* comentario em varias linhas
+por exemplo assim */
+
+ // declarando
+ //var nome;
+ //var idade;
+
+ //atribuindo
+ //nome = "um nome";
+ //idade = 30
+
+ //inicializando
+//var aula = "Aula 02";
+
+
+//contas
+//var numeroA = 2
+//var numeroB = 3
+//var numeroC = 5
+
+//var resultadoDaSoma = numeroA + numeroB
+//var resultadoDaDivisao = nume - numeroA
+
+var nome = "Juan"
+var sobrenome = "Zimmermann"
+var ano = 2024
+var idade = 27
+
+var nomeCompleto = nome + sobrenome
+var idadeNome = idade + nome
+
+var nome = prompt("insira o nome aqui")
+var resultado = nome + " " + "Seja Bem-vindo";
+alert(resultado);
+
+var valor = prompt("insira um valor a ser multiplicado por 7")
+var valorNumerico = parseInt(7);
+var resultadoNumerico = valor * valorNumerico;
+alert(resultadoNumerico);
+
+var nome = prompt("insira seu 1º nome aqui")
+var sobrenome = prompt("insira sobrenome aqui")
+var resultadoNomeCompleto = nome + " " + sobrenome;
+alert(resultadoNomeCompleto);
+
+
+console.log(inserirNome)
+console.log("bem - vindo")
