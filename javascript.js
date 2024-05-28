d99d001e2734595ac2cd34217e27dbaf8a584835
Juan Zimmerman de Souza Oliveira <juanito.tevez@gmail.com>
2024-05-28 19:31:10 -0300

    desafio3

--- javascript.js
+++ javascript.js
@@ -23,28 +23,50 @@ por exemplo assim */
 //var resultadoDaSoma = numeroA + numeroB
 //var resultadoDaDivisao = nume - numeroA
 
-var nome = "Juan"
-var sobrenome = "Zimmermann"
-var ano = 2024
-var idade = 27
+//var nome = "Juan"
+//var sobrenome = "Zimmermann"
+//var ano = 2024
+//var idade = 27
 
-var nomeCompleto = nome + sobrenome
-var idadeNome = idade + nome
+//var nomeCompleto = nome + sobrenome
+//var idadeNome = idade + nome
 
-var nome = prompt("insira o nome aqui")
-var resultado = nome + " " + "Seja Bem-vindo";
-alert(resultado);
+//var nome = prompt("insira o nome aqui")
+//var resultado = nome + " " + "Seja Bem-vindo";
+//alert(resultado);
 
-var valor = prompt("insira um valor a ser multiplicado por 7")
-var valorNumerico = parseInt(7);
-var resultadoNumerico = valor * valorNumerico;
-alert(resultadoNumerico);
+//var valor = prompt("insira o custo a ser calculado com icms de SP")
+//var valorNumerico = 1.18;
+//var resultadoNumerico = valor * valorNumerico;
+//alert(resultadoNumerico);
 
-var nome = prompt("insira seu 1º nome aqui")
-var sobrenome = prompt("insira sobrenome aqui")
-var resultadoNomeCompleto = nome + " " + sobrenome;
-alert(resultadoNomeCompleto);
+//var custo = prompt("Com qual margem deseja vender o produto ?")
+//var resultadoCusto = parseInt(custo) * resultadoNumerico ;
+//alert(resultadoNomeCompleto);
+
+//console.log(inserirNome)
+//console.log("bem - vindo")
 
 
-console.log(inserirNome)
-console.log("bem - vindo")
+//3º Desafio
+
+// Solicita a quantidade de parcelas
+let quantidadeParcelas = prompt("Digite a quantidade de parcelas:");
+let custoTotal = prompt("Digite o valor");
+
+
+// Convertendo a entrada para número
+quantidadeParcelas = parseInt(quantidadeParcelas);
+custoTotal = parseInt(custoTotal);
+
+// Valor fixo para exemplo
+let valorTotal = custoTotal; // Valor total a ser parcelado
+
+// Calcula o valor de cada parcela
+let valorParcela = valorTotal / quantidadeParcelas;
+
+// Loop for para exibir o valor de cada parcela
+for (let i = 1; i <= quantidadeParcelas; i++) {
+    console.log("Parcela " + i + ": R$" + valorParcela.toFixed(2));
+    alert("Parcela " + i + ": R$" + valorParcela.toFixed(2))
+}
