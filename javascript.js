b2f14a502d69419d90c9670c618a781a4d15987c
Juan Zimmerman de Souza Oliveira <juanito.tevez@gmail.com>
2024-05-28 22:59:46 -0300

    teste desafio3

--- javascript.js
+++ javascript.js
@@ -1,54 +1,47 @@
-// Comentario em uma linha
+ /*Comentario em uma linha
+ comentario em varias linhas
+por exemplo assim 
 
-/* comentario em varias linhas
-por exemplo assim */
-
- // declarando
- //var nome;
- //var idade;
-
- //atribuindo
- //nome = "um nome";
- //idade = 30
-
- //inicializando
-//var aula = "Aula 02";
+ declarando
+var nome;
+var idade;
+atribuindo
+nome = "um nome";
+idade = 30
+inicializando
+var aula = "Aula 02";
 
 
-//contas
-//var numeroA = 2
-//var numeroB = 3
-//var numeroC = 5
+contas
+var numeroA = 2
+var numeroB = 3
+var numeroC = 5
 
-//var resultadoDaSoma = numeroA + numeroB
-//var resultadoDaDivisao = nume - numeroA
+var resultadoDaSoma = numeroA + numeroB
+var resultadoDaDivisao = nume - numeroA
+var nome = "Juan"
+var sobrenome = "Zimmermann"
+var ano = 2024
+var idade = 27
 
-//var nome = "Juan"
-//var sobrenome = "Zimmermann"
-//var ano = 2024
-//var idade = 27
+var nomeCompleto = nome + sobrenome
+var idadeNome = idade + nome
+var nome = prompt("insira o nome aqui")
+var resultado = nome + " " + "Seja Bem-vindo";
+alert(resultado);
+var valor = prompt("insira o custo a ser calculado com icms de SP")
+var valorNumerico = 1.18;
+var resultadoNumerico = valor * valorNumerico;
+alert(resultadoNumerico);
+var custo = prompt("Com qual margem deseja vender o produto ?")
+var resultadoCusto = parseInt(custo) * resultadoNumerico ;
+alert(resultadoNomeCompleto);
 
-//var nomeCompleto = nome + sobrenome
-//var idadeNome = idade + nome
-
-//var nome = prompt("insira o nome aqui")
-//var resultado = nome + " " + "Seja Bem-vindo";
-//alert(resultado);
-
-//var valor = prompt("insira o custo a ser calculado com icms de SP")
-//var valorNumerico = 1.18;
-//var resultadoNumerico = valor * valorNumerico;
-//alert(resultadoNumerico);
-
-//var custo = prompt("Com qual margem deseja vender o produto ?")
-//var resultadoCusto = parseInt(custo) * resultadoNumerico ;
-//alert(resultadoNomeCompleto);
-
-//console.log(inserirNome)
-//console.log("bem - vindo")
+console.log(inserirNome)
+console.log("bem - vindo")
 
 
-//3º Desafio
+//3º Desafio*/
 
 // Solicita a quantidade de parcelas
 let quantidadeParcelas = prompt("Digite a quantidade de parcelas:");
