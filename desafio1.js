158d6f00f3bebcd3c05840e7ca136e07e05a44b8
Juan Zimmerman <juan.zimmerman@icloud.com>
2024-06-04 20:47:36 -0300

    melhora da pag

---
+++ desafio1.js
@@ -0,0 +1,14 @@
+
+var nome = prompt("insira o nome aqui")
+var resultado = nome + " " + "Seja Bem-vindo";
+alert(resultado);
+var valor = prompt("insira o custo a ser calculado com icms de SP")
+var valorNumerico = 1.18;
+var resultadoNumerico = valor * valorNumerico;
+alert(resultadoNumerico);
+var custo = prompt("Com qual margem deseja vender o produto ?")
+var resultadoCusto = parseInt(custo) * resultadoNumerico ;
+alert(resultadoNomeCompleto);
+
+console.log(inserirNome)
+console.log("bem - vindo")
